3ae6ddd6092bd33597827a8990c517742e35152a
ajrnnn <muriithia05@gmail.com>
2025-07-13 20:59:39 +0300

    Update Abstract.java

--- java-exercises-main/src/AbstractClasses/Abstract.java
+++ java-exercises-main/src/AbstractClasses/Abstract.java
@@ -3,7 +3,7 @@ package AbstractClasses;
 public class Abstract {
     public static void main(String[] args) {
         Admin currentAdmin = new Admin(1227, "Alex", "Alex@123");
-        User currentUser = new User(7669, "Kayla");
+        User currentUser = new User(7669, "Manu");
         currentAdmin.performOperation(new Update());
         currentUser.performOperation(new View());
 
